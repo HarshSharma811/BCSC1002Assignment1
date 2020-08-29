efb9765ef18e7087cf8bf6a3481f045c0894c07c
HarshSharma811 <harsh.sharma1_cs19@gla.ac.in>
2020-08-29 17:25:53 -0400

    constructor created with parameter and without parameter

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -11,4 +11,16 @@ public class Book {
     private String bookName;
     private String authorName;
     private String ISBNBookNumber;
+
+    public Book() {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.ISBNBookNumber = ISBNBookNumber;
+    }
+
+    public Book(String bookName, String authorName, String ISBNBookNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.ISBNBookNumber = ISBNBookNumber;
+    }
 }
