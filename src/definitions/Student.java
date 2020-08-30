daa91d161b77751086c277348911060ea51da776
HarshSharma811 <harsh.sharma1_cs19@gla.ac.in>
2020-08-30 01:05:30 -0400

    Non-parameterised and Parameterised constructor created .

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -10,7 +10,25 @@ public class Student {
     private String firstName;
     private String middleName;
     private String lastName;
-    private long universityRollNumnber;
+    private long universityRollNumber;
     private int bookIssuedByStudent;
     private String[] books;
+
+    public Student() {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.universityRollNumber = universityRollNumber;
+        this.bookIssuedByStudent = bookIssuedByStudent;
+        this.books = new String[5];
+    }
+
+    public Student(String firstName, String middleName, String lastName, long universityRollNumber, int bookIssuedByStudent) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.universityRollNumber = universityRollNumber;
+        this.bookIssuedByStudent = bookIssuedByStudent;
+        this.books = new String[5];
+    }
 }
