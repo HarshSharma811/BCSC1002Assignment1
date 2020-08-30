c39910c68d5bfe24a49781573d7bcb992f9c3fd4
HarshSharma811 <harsh.sharma1_cs19@gla.ac.in>
2020-08-30 15:25:38 -0400

    created front desk menu.

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -10,6 +10,12 @@ public class FrontDesk {
     public static void main(String[] args) {
         Student info = new Student();
         info.studentInfo();
-
+        System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+        System.out.println("How may I help you today?");
+        System.out.println("1. Issue a new book for me. ");
+        System.out.println("2. Return a previously issued book for me. ");
+        System.out.println("3. Show me all my issued book. ");
+        System.out.println("4. Exit. ");
+        System.out.println("Enter the option: ");
     }
 }
