b9611a42e78fd4047834060143cd4845f738b6bf
HarshSharma811 <harsh.sharma1_cs19@gla.ac.in>
2020-08-30 00:55:23 -0400

    created private fields.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,4 +7,10 @@
 package definitions;
 
 public class Student {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private long universityRollNumnber;
+    private int bookIssuedByStudent;
+    private String[] books;
 }
